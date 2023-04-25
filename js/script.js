a7eac3034c067b42e83d387460d38d987be6ba2d
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-04-25 21:00:41 +0000

    hopefully it works this time

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function generates a random number between 1 and 6
+ */
+const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
+
+/**
+ * This function updates the slider value.
+ */
+function myButtonClicked() {
+  // input
+  const guessedNumber = parseFloat(
+    document.getElementById("guessed-number").value
+  )
+
+  // process
+  if (guessedNumber == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was " + randomNumber + " you got it!"
+  }
+
+  if (guessedNumber != randomNumber) {
+    document.getElementById("answer").innerHTML = "Wrong answer. Try again"
+  }
+}
